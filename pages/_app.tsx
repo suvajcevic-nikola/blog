1a9d053e0e888c3328ae02377942aacfbb4f3d81
suvajcevic <suvajcevic1312@gmail.com>
2024-06-28 14:21:16 +0200

    Vercel Analytics :: Production Mode

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { Analytics } from "@vercel/analytics/react";
 import { ErrorBoundary, Layout } from "@/components";
 
 export default function App({ Component, pageProps }: AppProps) {
@@ -7,6 +8,7 @@ export default function App({ Component, pageProps }: AppProps) {
     <Layout>
       <ErrorBoundary>
         <Component {...pageProps} />
+        <Analytics mode={"production"} />
       </ErrorBoundary>
     </Layout>
   );
